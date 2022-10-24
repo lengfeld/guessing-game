8f67856a782f61b953f6447d04b3aaeeea6adfdb
Stefan Lengfeld <stefan.lengfeld@inovex.de>
2022-10-24 15:36:13 +0200

    Revert "my changes"
    This reverts commit 470d1e61a8d6abd624a5c8e144556ac8574de1c1.

--- guess.py
+++ guess.py
@@ -4,22 +4,22 @@ import sys
 from random import randrange
 
 def play():
-    number = randrange(-100, 100)
-    print("I have chosen a number.")
+    number = randrange(0, 10)
+    print("I have chosen a nomber.")
 
     while True:
         try:
-            guess = int(input("Your guess: "))
+            guess = int(input("Your guesss: "))
         except ValueError:
             print("Error. You have not entered a number. Try again!")
             continue
 
         if guess < number:
-            print("Your guess was too low!")
+            print("Your guess was to low!")
         elif guess > number:
-            print("Your guess was too high!")
+            print("Your guess was to high!")
         else:
-            print("Congratulations. Your guess is correct.")
+            print("Congratulations. Your guess was correct.")
             break;
 
 
@@ -28,7 +28,7 @@ def main():
         play()
 
         yes_or_no = input("Do you want to play again (y/n)?: ")
-        if yes_or_no not in ('yes', 'Yes', 'YES','y','Y'):
+        if yes_or_no not in ('y', 'Y'):
                 break
 
     return 0
