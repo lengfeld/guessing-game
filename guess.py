9b07ea986e3a06911170ab98184c7466c31521bd
Stefan Lengfeld <stefan.lengfeld@inovex.de>
2022-10-24 15:40:10 +0200

    Revert "my changes"
    This reverts commit 3dd873adb5632e1bfe7c302b429a61eb20169a25.

--- guess.py
+++ guess.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import sygue
+import sys
 from random import randrange
 
 def play():
-    number = randrange(0-100, 100)
-    print("I have chosen a number.")
+    number = randrange(0, 10)
+    print("I have chosen a nomber.")
 
     while True:
         try:
-            guess = int(input("Your guess: "))
+            guess = int(input("Your guesss: "))
         except ValueError:
             print("Error. You have not entered a number. Try again!")
             continue
